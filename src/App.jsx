f8baa3c53d4cb5e54e86edc5e3bdb1687ee5accd
Andres Felipe <Andresfp29032001@gmail.com>
2022-02-06 16:08:17 -0500

    Modal component ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
 import  Header from './components/Header';
+import Modal from './components/Modal';
+
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 const App = ()  => {
 
   //Hooks
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+
+  //
+  const handleNuevoGasto = () => {
+    setModal(true);
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500)
+  }
 
   return (
     <div>
@@ -15,6 +29,24 @@ const App = ()  => {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {/*si es valido muestre el botón*/}
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img 
+            src={IconoNuevoGasto}
+            alt="Icono nuevo gasto" 
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+
+      {/*si true => muestra el modal*/}  
+      {modal && <Modal 
+                  setModal={setModal} 
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                />}
     </div>
   )
 }
